9a06a3430d0344ed0dc8e654cc03270ed4741f33
AndreiKastsiukovich <andreikastsiukovich@gmail.com>
2023-02-01 14:22:54 +0300

    Commit-1

---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const Header = () => {
+    return <header className='header'>
+                 <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCUEwts4uoA2Zk4uGFSWayTNfoLgraSoxF2g&usqp=CAU'/>
+            </header>
+
+}
