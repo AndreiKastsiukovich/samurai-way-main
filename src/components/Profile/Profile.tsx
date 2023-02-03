3cbcbff4178fae1ce110ac3376eba648c85c3bdb
AndreiKastsiukovich <andreikastsiukovich@gmail.com>
2023-02-03 18:58:21 +0300

    Commit-1

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -3,7 +3,7 @@ import classes from './Profile.module.css';
 import {MyPosts} from "./MyPosts/MyPosts";
 
 export const Profile = () => {
-    return <div className={classes.content}>
+    return <div>
         <div>
             <img src='https://images.pexels.com/photos/4613547/pexels-photo-4613547.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'/>
         </div>
