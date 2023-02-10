c151e219bc5c62d45b0b0ba069883b388e446c38
AndreiKastsiukovich <andreikastsiukovich@gmail.com>
2023-02-10 18:40:18 +0300

    Commit-1

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,32 @@ import classes from './Dialogs.module.css';
 
 export const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <div className={classes.dialog + ' ' + classes.active}>
+                    Andrey
+                </div>
+                <div className={classes.dialog}>
+                    Kirill
+                </div>
+                <div className={classes.dialog}>
+                    Dimon
+                </div>
+                <div className={classes.dialog}>
+                    Vadim
+                </div>
+                <div className={classes.dialog}>
+                    Sergey
+                </div>
+                <div className={classes.dialog}>
+                    Viktor
+                </div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>Hi</div>
+                <div className={classes.message}>How are you ?</div>
+                <div className={classes.message}>Yo</div>
+            </div>
         </div>
 
     )
