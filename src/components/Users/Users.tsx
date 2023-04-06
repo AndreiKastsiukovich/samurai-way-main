1349f00d9ae4b65e34cc40325778b6645f096f54
AndreiKastsiukovich <andreikastsiukovich@gmail.com>
2023-04-06 17:17:13 +0300

    Commit-1

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -8,14 +8,18 @@ type UsersPropsType = UsersContainerType
 
 export const Users = (props: UsersPropsType) => {
 
-    if (props.users.length === 0) {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-            props.setUsers(response.data.items)
-        })
+    const getUsers = () => {
+        if (props.users.length === 0) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+                props.setUsers(response.data.items)
+            })
+        }
     }
 
     return (
         <div>
+            <button onClick={getUsers}>Get users</button>
+
             {props.users.map((el) => {
                 return (
                     <div key={el.id}>
